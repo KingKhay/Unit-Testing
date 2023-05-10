a6bc53be2638b34ace2d1e008d33ca554c03425d
KingKhay <ebenezerbuabeng60@gmail.com>
2023-05-10 08:44:54 +0000

    initial commit

---
+++ src/test/java/com/khaydev/UnitTesting/UnitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khaydev.UnitTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
