3b7aa14fc8b2b76ed88569fa2d72272fb1436a3b
KingKhay <ebenezerbuabeng60@gmail.com>
2023-05-11 08:58:15 +0000

    wrote unit test for save student service method

--- src/test/java/com/khaydev/UnitTesting/UnitTestingApplicationTests.java
+++ src/test/java/com/khaydev/UnitTesting/UnitTestingApplicationTests.java
@@ -1,13 +1,44 @@
 package com.khaydev.UnitTesting;
 
+import com.khaydev.UnitTesting.model.Student;
+import com.khaydev.UnitTesting.repository.StudentRepository;
+import com.khaydev.UnitTesting.service.StudentService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class UnitTestingApplicationTests {
 
+	@Autowired
+	StudentService studentService;
+
+	@MockBean
+	StudentRepository studentRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	@DisplayName("Save Method Must Return the Student Saved")
+	public void testSaveStudentServiceMethod(){
+		Student student = new Student(1,"Khay","Takoradi",null,null,null);
+
+		when(studentRepository.save(student)).thenReturn(student);
+
+		Student theSavedStudent = studentService.createStudent(student);
+
+		assertEquals(student.getName(), theSavedStudent.getName());
+		assertEquals(student.getAddress(), theSavedStudent.getAddress());
+
+		//Verify that the studentRepository.save method run only once
+		verify(studentRepository, times(1)).save(student);
 	}
 
 }
